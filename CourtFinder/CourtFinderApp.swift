15bdfaeb94716ca871f8a4fe7ea7866860e72e25
Rajat Khare <rdkhare123@gmail.com>
2024-07-09 21:16:31 +0530

    Initial Commit

---
+++ CourtFinder/CourtFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourtFinderApp.swift
+//  CourtFinder
+//
+//  Created by Rajat Khare on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CourtFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
